2876a3f55dcc07d2c6acbc83f13fa116fcfed6eb
Jeremy Batterbee <jeremybatterbee@gmail.com>
2020-11-12 19:40:45 -0600

    database connectivity

---
+++ FinalProject/FinalProject/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SQLite;
+
+namespace FinalProject {
+    public class Event {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Reminder { get; set; }
+    }
+}
