cbeef61ff6421a3b87f5ff16addde0f962a7466b
Jeremy Batterbee <jeremybatterbee@gmail.com>
2020-12-07 16:50:47 -0600

    calendar update

--- FinalProject/FinalProject/MainPage.xaml.cs
+++ FinalProject/FinalProject/MainPage.xaml.cs
@@ -6,30 +6,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamForms.Controls;
 
 namespace FinalProject
 {
     public partial class MainPage : ContentPage
     {
-        List<XamForms.Controls.SpecialDate> Events;
-
         public MainPage()
         {
             InitializeComponent();
-            List<XamForms.Controls.SpecialDate> Events = new List<XamForms.Controls.SpecialDate>();
-
-            foreach (Event e in App.Database.GetAllEventsAsync().Result) {
-                Events.Add(new XamForms.Controls.SpecialDate(e.StartTime) {
-                    BackgroundColor = Color.Red
-                });
-            }
+            BindingContext = new CalendarViewModel();
         }
 
         public async void EventList(object sender, EventArgs args) {
-            ListPage lPage = new ListPage();
-            Date sDate = new Date { SelectedDateTime = calendar.SelectedDate };
-            lPage.BindingContext = sDate;
-            await Navigation.PushAsync(lPage);
+            if (calendar.SelectedDate != null) {
+                ListPage lPage = new ListPage();
+                Date sDate = new Date { SelectedDateTime = calendar.SelectedDate };
+                lPage.BindingContext = sDate;
+                await Navigation.PushAsync(lPage);
+            }
+        }
+
+        protected override void OnAppearing() {
+            ((CalendarViewModel)BindingContext).Initialize();
         }
     }
 }
